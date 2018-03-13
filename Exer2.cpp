a4dc40e29ae89cd99e6130178d262af2856d8561
baddi youssef <baddiyoussef85@gmail.com>
2018-03-13 09:13:20 +0000

    solution exercice 2

---
+++ Exer2.cpp
@@ -0,0 +1,18 @@
+//
+// Created by baddi youssef on 3/13/18.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int lar;
+    int lon;
+    cout<<"Veuillez entrer la longeur"<<endl;
+    cin>>lon;
+    cout<<"Veuillez entrer la largeur"<<endl;
+    cin>>lar;
+    cout<<"la surface est "<< lon * lar<<endl;
+}
+
