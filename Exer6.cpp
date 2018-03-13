7f7a55c280de2c097ad5eedef7ccfaca3ac5b84e
baddi youssef <baddiyoussef85@gmail.com>
2018-03-13 09:58:53 +0000

    solution exercice 6

---
+++ Exer6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int z,n;
+	z=1;
+	cout << "entrer un nombre" << endl;
+	cin>> n ;
+	if ((n>=56) and (n<=78) )
+		cout<<" GAGNE" << endl ;
+	else
+		cout<<"PERDU" << endl ;
+
+    return 0;
+}
